5eb52cef50b5a8556192e5861ba1b8329497f116
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2021-04-08 17:27:43 -0400

    ✨ Load module into tickets

--- src/modules/tickets/tickets.module.ts
+++ src/modules/tickets/tickets.module.ts
@@ -1,10 +1,11 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { QuestionsModule } from '../questions/questions.module';
 import { UsersModule } from '../users/users.module';
 import { TicketService } from './services/ticket.service';
 
 @Module({
-  imports: [UsersModule, HttpModule, ConfigModule],
+  imports: [UsersModule, HttpModule, ConfigModule, QuestionsModule],
   providers: [TicketService],
   exports: [TicketService]
 })
