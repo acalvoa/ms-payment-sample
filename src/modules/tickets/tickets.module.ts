fbe6b9894f911d9fbbc95a48c7b88a8e4f2d4498
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2021-05-04 19:14:45 -0400

    ✨ Add shared module

--- src/modules/tickets/tickets.module.ts
+++ src/modules/tickets/tickets.module.ts
@@ -1,11 +1,13 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SharedModule } from 'src/shared/shared.module';
 import { QuestionsModule } from '../questions/questions.module';
 import { UsersModule } from '../users/users.module';
 import { TicketService } from './services/ticket.service';
 
 @Module({
-  imports: [UsersModule, HttpModule, ConfigModule, QuestionsModule],
+  imports: [UsersModule, HttpModule, ConfigModule, SharedModule, 
+    QuestionsModule],
   providers: [TicketService],
   exports: [TicketService]
 })
